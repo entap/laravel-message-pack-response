2404f75b971948878e8c1fa9f71d147aa227e134
ii masamitsu <ii@entap.co.jp>
2021-03-23 10:00:29 +0900

    レスポンスマクロを登録する

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,6 +1,9 @@
 <?php
 namespace Entap\Laravel\Response\MessagePack;
 
+use MessagePack\Packer;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
@@ -12,5 +15,14 @@ class ServiceProvider extends BaseServiceProvider
 
     public function register()
     {
+        Response::macro('mpac', function ($data) {
+            if ($data instanceof Arrayable) {
+                $data = $data->toArray();
+            }
+            return response((new Packer())->pack($data))->header(
+                'Content-Type',
+                'application/x-msgpack'
+            );
+        });
     }
 }
